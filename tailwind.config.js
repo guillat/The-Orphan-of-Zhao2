dd5d95ee43e38153c3d9aef67b8f08cb4c5d4de5
gplans <guillem.plans@gmail.com>
2020-06-05 21:54:13 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ['./src/**/*.html'],
+  theme: {
+    colors: {
+      black: '#333',
+      lightGrey: '#f8f9fa',
+      yellow: '#fff952',
+      darkYellow: '#fff82c',
+      white: '#FFFFFF',
+      grey: '#ccc',
+      darkGrey: '#3c3c3b',
+    },
+    fontFamily: {
+      headings: ['Roboto', 'Sans-serif'],
+      text: ['Sanchez', 'Sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
